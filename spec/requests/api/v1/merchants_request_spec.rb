bce538ecb4fc7adf20bb6c032589cdbc58e10e4b
Eldridge Turambi <eturambi95@gmail.com>
2022-03-29 17:26:18 -0600

    Add: spec for json api- one merchant(show)

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Expose Merchants API' do
 
-  it 'sends a list of all merchants' do
+  it 'sends json for a list of all merchants' do
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
@@ -20,5 +20,22 @@ RSpec.describe 'Expose Merchants API' do
       expect(merchant[:attributes]).to have_key(:name)
       expect(merchant[:attributes][:name]).to be_a(String)
     end
+  end
+
+  it 'can send json for one merchant' do
+    merchant_id = create(:merchant).id
+
+    get "/api/v1/merchants/#{merchant_id}"
+
+    expect(response).to be_successful
+
+    json_merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(json_merchant).to have_key(:data)
+    expect(json_merchant[:data]).to have_key(:id)
+    expect(json_merchant[:data]).to have_key(:type)
+    expect(json_merchant[:data]).to have_key(:attributes)
+    expect(json_merchant[:data][:attributes]).to have_key(:name)
+    expect(json_merchant[:data][:attributes][:name]).to be_a(String)
   end
 end
