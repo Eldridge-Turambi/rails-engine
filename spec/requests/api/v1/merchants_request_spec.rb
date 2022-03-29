e05f5404169cc9acb3f3f547093c27d4616d62d6
Eldridge Turambi <eturambi95@gmail.com>
2022-03-29 16:58:12 -0600

    Add: spec for all merchants in merchants_request_spec

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,5 +8,17 @@ RSpec.describe 'Expose Merchants API' do
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+    expect(merchants[:data].count).to eq(3)
+
+    merchants[:data].each do |merchant|
+      expect(merchant).to have_key(:id)
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to eq('merchant')
+      expect(merchant).to have_key(:attributes)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
+    end
   end
 end
