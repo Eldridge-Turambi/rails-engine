c2407a714c115bd7128eaabfd67386b86e3b5ee0
Eldridge Turambi <eturambi95@gmail.com>
2022-03-30 17:27:53 -0600

    Routes for seeing item/merchant merchant show page

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,12 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: :merchant_items
       end
-      resources :items, only: [:index, :show, :create, :destroy, :update]
+      resources :items, only: [:index, :show, :create, :destroy, :update] do
+        resources :merchant, only: [:index], controller: :item_merchant
+      end
     end
+
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
