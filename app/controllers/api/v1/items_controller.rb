358f0045df30c014d2e1cab278cac8523e5665d5
Eldridge Turambi <eturambi95@gmail.com>
2022-03-30 16:46:33 -0600

    Create: Functionality in the controller

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,4 +8,28 @@ class Api::V1::ItemsController < ApplicationController
     render json: ItemSerializer.new(Item.find(params[:id]))
   end
 
+  def create
+    item = Item.new(item_params)
+
+    if item.save
+      render json: ItemSerializer.new(Item.create(item_params)), status: 201
+    else
+      render status: 404
+    end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+
+    if item.destroy
+      render status: :no_content
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
+
 end
