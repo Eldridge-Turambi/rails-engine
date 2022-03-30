87bc3f9827840046478324735121eb796ba6e11f
Eldridge Turambi <eturambi95@gmail.com>
2022-03-29 19:26:47 -0600

    Update: FactoryBot/Faker for items

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::Company.name}
+    name { Faker::Device.model_name}
+    description { Faker::Lorem.sentence(word_count: 3)}
+    unit_price { Faker::Number.number(digits: 4)}
   end
 end
